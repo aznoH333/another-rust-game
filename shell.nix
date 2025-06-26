805f3fbac0a561d848a032599b649c557cd52531
aznoH333 <glentik23@gmail.com>
2025-06-26 16:40:36 +0200

    got the game working

--- shell.nix
+++ shell.nix
@@ -8,11 +8,35 @@ in
       cargo
       rustc
       gcc
-      alsa-lib
-      alsa-lib.dev
       nix
-      udev
+alsa-lib
+              udev
+              #NOTE Add more deps
+              vulkan-loader
+             
+glfw-wayland 
+xorg.libX11
+        xorg.libX11.dev
+        xorg.libXcursor
+        xorg.libXi
+        xorg.libXinerama
+        xorg.libXrandr            
+          vulkan-headers
+          libxkbcommon
       ];
-nativeBuildInputs = with pkgs; [ pkg-config ];
-
+nativeBuildInputs = with pkgs; [ 
+udev alsa-lib vulkan-loader
+    libxkbcommon wayland # To use the wayland feature
+glfw-wayland
+pkg-config
+];
+# Make sure the graphics libraries are findable in our dev shell.
+  shellHook = ''
+    export LD_LIBRARY_PATH="${lib.makeLibraryPath [
+      "/run/opengl-driver"
+      "/run/opengl-driver-32"
+      libGL
+      vulkan-loader
+    ]}:$LD_LIBRARY_PATH"
+  '';
   }
